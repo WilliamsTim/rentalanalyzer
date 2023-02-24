9426d35039120c5aa84a3ae629285a61ee3b561b
Timothy <timothy@Timothys-MacBook-Air.local>
2023-02-23 22:06:37 -0600

    Begin set up of front end with material ui. Will migrate mortgage calculator over next

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { styled } from '@mui/system';
 
+const Flex = styled('div')({
+  display: 'flex',
+  height: '100vh',
+  alignItems: 'center',
+  justifyContent: 'center'
+})
+const MyComponent = styled('div')({
+  color: 'black',
+  backgroundColor: '#EAE0C8',
+  padding: 8,
+  borderRadius: 30,
+  width: '28em',
+  height: '32em',
+});
 
 function App() {
   return (
-    <div>App</div>
+    <Flex>
+      <MyComponent>App</MyComponent>
+    </Flex>
   )
 }
 
