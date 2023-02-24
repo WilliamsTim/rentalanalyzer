cc1395ccec162a604bcc3c33536ebd70b2035322
Timothy <timothy@Timothys-MacBook-Air.local>
2023-02-24 16:40:44 -0600

    Begin working on styling and implementing the home page and adding various svgs and input elements

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,26 +1,85 @@
-import React from 'react'
-import { styled } from '@mui/system';
-
-const Flex = styled('div')({
-  display: 'flex',
-  height: '100vh',
-  alignItems: 'center',
-  justifyContent: 'center'
-})
-const Container = styled('div')({
-  color: 'black',
-  backgroundColor: 'white',
-  padding: 8,
-  borderRadius: 30,
-  width: '28em',
-  height: '32em',
-});
+import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import House from '../images/House.png';
 
 function Home() {
   return (
-    <Flex>
-      <Container className="container">App</Container>
-    </Flex>
+    <div className='outerContainer'>
+      <div className="container">
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            px: 2,
+          }}
+        >
+          <img src={House} alt={House} className='photo' />
+          <TextField
+            required
+            id="standard-required"
+            label="Amount"
+            variant="standard"
+            sx={{
+              mt: -2,
+            }}
+          />
+          <TextField
+            required
+            id="standard-required"
+            label="Percentage"
+            variant='standard'
+            sx={{
+              mt: 1,
+            }}
+          />
+          <TextField
+            required
+            id="standard-required"
+            label="Interest"
+            variant='standard'
+            sx={{
+              mt: 1,
+            }}
+          />
+          <TextField
+            id="standard"
+            label="Property Tax"
+            variant='standard'
+            sx={{
+              mt: 1,
+            }}
+          />
+                    <TextField
+            id="standard"
+            label="Home Association"
+            variant='standard'
+            sx={{
+              mt: 1,
+            }}
+          />
+                    <TextField
+            id="standard"
+            label="Home Insurance"
+            variant='standard'
+            sx={{
+              mt: 1,
+            }}
+          />
+                    <TextField
+            id="standard"
+            label="Private Mortgage Insurance"
+            variant='standard'
+            sx={{
+              mt: 1,
+            }}
+          />
+        </Box>
+      </div>
+    </div>
   )
 }
 
