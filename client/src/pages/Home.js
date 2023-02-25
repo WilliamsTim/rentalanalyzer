c17614649ffdae7e6792c0c878b75d7fa67266b9
Timothy <timothy@Timothys-MacBook-Air.local>
2023-02-24 18:09:59 -0600

    Set up all styles and looks for home page. Now need to integrate backend and routing to rental view and to set up form validation and RentalView component. Then I will be all finished, finally!

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import House from '../images/House.png';
 
 function Home() {
+  // variable definitions
+  const [boxOneSlider, setBoxOneSlider] = useState('0');
+  const [boxTwoSlider, setBoxTwoSlider] = useState('750');
+
+  // function definitions
+  const handleClick = (e) => {
+    e.preventDefault();
+    setBoxOneSlider('-750');
+    setBoxTwoSlider('0');
+  }
   return (
     <div className='outerContainer'>
       <div className="container">
@@ -11,20 +23,30 @@ function Home() {
           component="form"
           noValidate
           autoComplete="off"
+          className='slidebox'
           sx={{
             display: 'flex',
             flexDirection: 'column',
             px: 2,
+            width: '100%',
+            position: 'absolute',
+            left: `${boxOneSlider}px`,
           }}
         >
           <img src={House} alt={House} className='photo' />
+          <Typography
+            sx={{
+              mx: 'auto',
+              mt: -1,
+            }}
+          >Required Fields</Typography>
           <TextField
             required
             id="standard-required"
             label="Amount"
             variant="standard"
             sx={{
-              mt: -2,
+              mt: 0,
             }}
           />
           <TextField
@@ -46,14 +68,63 @@ function Home() {
             }}
           />
           <TextField
-            id="standard"
-            label="Property Tax"
+            required
+            id="standard-required"
+            label="Estimated Rent"
+            variant="standard"
+            sx={{
+              mt: 1,
+            }}
+          />
+          <TextField
+            required
+            id="standard-required"
+            label="Target Profit Per Month"
             variant='standard'
             sx={{
               mt: 1,
             }}
           />
-                    <TextField
+          <Button
+            variant="outlined"
+            onClick={handleClick}
+            sx={{
+              mt: 3,
+            }}
+          >
+            Next
+          </Button>
+        </Box>
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+          className='slidebox'
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            px: 2,
+            width: '100%',
+            position: 'absolute',
+            left: `${boxTwoSlider}px`,
+          }}
+        >
+          <img src={House} alt={House} className='photo' />
+          <Typography
+            sx={{
+              mx: 'auto',
+              mt: -1,
+            }}
+          >Optional Fields</Typography>
+          <TextField
+            id="standard"
+            label="Property Tax"
+            variant='standard'
+            sx={{
+              mt: -2,
+            }}
+          />
+          <TextField
             id="standard"
             label="Home Association"
             variant='standard'
@@ -61,7 +132,7 @@ function Home() {
               mt: 1,
             }}
           />
-                    <TextField
+          <TextField
             id="standard"
             label="Home Insurance"
             variant='standard'
@@ -69,7 +140,7 @@ function Home() {
               mt: 1,
             }}
           />
-                    <TextField
+          <TextField
             id="standard"
             label="Private Mortgage Insurance"
             variant='standard'
@@ -77,6 +148,14 @@ function Home() {
               mt: 1,
             }}
           />
+          <Button
+            variant="outlined"
+            sx={{
+              mt: 3,
+            }}
+          >
+            Go
+          </Button>
         </Box>
       </div>
     </div>
